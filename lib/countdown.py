980b1e5d3c4d1f5ad9c1b89deb409055ea05a0dc
Vahan Nadjarian <vahan@nadjarian.com>
2023-05-22 17:17:32 -0400

    complete lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,13 @@
 # your code goes here!
+def countdown_with_sleep(num):
+    while num > 0:
+        print(f'{num} SECOND(S)!')
+        num -= 1
+    print('HAPPY NEW YEAR!')
+    
+
+def countdown(num):
+    while num > 0:
+        print(f'{num} SECOND(S)!')
+        num -= 1
+    print('HAPPY NEW YEAR!')
